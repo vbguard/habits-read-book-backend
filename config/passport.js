ff1ee5ac5fbb3511e5f95dcdbab6af398a35990e
Viktor Bulvarenko <viktor.bulvarenko@gmail.com>
2019-10-30 21:09:07 +0200

    start rewrite swager doc

--- config/passport.js
+++ config/passport.js
@@ -68,49 +68,6 @@ module.exports = function(passport) {
     });
   });
 
-  // passport.use(
-  //   new FacebookStrategy(
-  //     {
-  //       clientID: '',
-  //       clientSecret: '',
-  //       callbackURL: `https://domain/api/auth/facebook/callback`
-  //     },
-  //     async (accessToken, refreshToken, profile, done) => {
-  //       try {
-  //         const user = await User.findOne({ facebookId: profile.id });
-  //         if (user) return done(err, user);
-  //         if (!user) {
-  //           const newUser = await new User({
-  //             githubId: profile._json.id,
-  //             name: profile._json.name
-  //           });
-
-  //           newUser.save((err, user) => done(err, user));
-  //         }
-  //       } catch (error) {
-  //         done(error, null);
-  //       }
-  //     }
-  //   )
-  // );
-
-  // passport.use(
-  //   new OAuth2Strategy(
-  //     {
-  //       authorizationURL: 'http:///oauth2/authorize',
-  //       tokenURL: 'https://www.example.com/oauth2/token',
-  //       clientID: EXAMPLE_CLIENT_ID,
-  //       clientSecret: EXAMPLE_CLIENT_SECRET,
-  //       callbackURL: 'http://localhost:3000/auth/example/callback'
-  //     },
-  //     ((accessToken, refreshToken, profile, cb) => {
-  //   User.findOrCreate({ exampleId: profile.id }, function (err, user) {
-  //     return cb(err, user);
-  //   });
-  // })
-  //   )
-  // );
-
   passport.use(
     new GoogleStrategy(
       {
